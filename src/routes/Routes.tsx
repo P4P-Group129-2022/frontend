dc2a23a915ec48c02253cc3fc36ad84b09e7fd5b
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-06-23 13:35:58 +1200

    Add terminal page with limited functionality

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes as Switch } from "react-router-dom";
 import Dock from "../components/Dock";
 import DemoPage from "../pages/DemoPage/DemoPage";
 import HomePage from "../pages/HomePage/HomePage";
+import TerminalPage from "../pages/TerminalPage/TerminalPage";
 
 function Routes() {
   return (
@@ -11,7 +12,7 @@ function Routes() {
 
         <Route path="play" element={<Dock />}>
           <Route path="demo" element={<DemoPage />} />
-
+          <Route path="terminal" element={<TerminalPage />} />
           {/* All unregistered routes inside play direct to play page. */}
           <Route path="*" element={<Navigate to="/play" />} />
         </Route>
