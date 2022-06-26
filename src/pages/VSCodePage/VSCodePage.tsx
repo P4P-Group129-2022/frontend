aef6f64d6b97e47e61264b6b693c0f67d268688e
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-06-26 21:43:01 +1200

    Change editor language and minor styling

--- src/pages/VSCodePage/VSCodePage.tsx
+++ src/pages/VSCodePage/VSCodePage.tsx
@@ -2,17 +2,33 @@ import React, { useState } from "react";
 import Editor from "@monaco-editor/react";
 
 function VSCodePage() {
-  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
+  const [code, setCode] = useState(`class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        int i = 0;
+        for (int j = 0; j < t.length(); j++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                if (i >= s.length()) return true;
+            }
+        }
+        return false;
+    }
+}`);
 
   return (
     <div
       style={{
         width: "100vw",
+        height: "100%"
       }}
     >
       <Editor
-        height="70vh"
-        defaultLanguage="javascript"
+        options={{
+          fontSize: "14px",
+        }}
+        height="100%"
+        defaultLanguage="java"
         theme="vs-dark"
         defaultValue={code}
         onChange={(value) => value && setCode(value)}
