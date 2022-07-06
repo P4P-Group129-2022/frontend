55ea2000ffe398ab5b642cb2238a7dd73835a73a
Dave Shin <100652749+hiin3d55@users.noreply.github.com>
2022-07-07 09:50:11 +1200

    Now fetches scenario from the backend.

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,9 +1,15 @@
 import axios, {AxiosResponse} from 'axios';
 import {NotificationContent} from "../types/NotificationTypes";
+import {ScenarioContent} from "../types/ScenarioTypes";
 
 const API_ENDPOINT = `${process.env.REACT_APP_BACKEND_ENDPOINT}`;
 
 export const getNotificationByName: (notificationName: string) =>
     Promise<AxiosResponse<{notificationFromDB: NotificationContent[]}>> = async (notificationName: string) => {
         return await axios.get(`${API_ENDPOINT}/api/notification/${notificationName}`);
+}
+
+export const getScenarioByNameId: (scenarioNameId: string) =>
+    Promise<AxiosResponse>{scenarioFromDB: ScenarioContent[]}>> = async (scenarioNameId: string) => {
+        return await axios.get(`${API_ENDPOINT}/api/scenario/${scenarioNameId}`);
 }
--- src/types/ScenarioTypes.ts
+++ src/types/ScenarioTypes.ts
@@ -1,6 +1,14 @@
 import { ChatDialog } from "./ChatTypes";
+import {NotificationContent} from "./NotificationTypes";
 
 export type ScenarioSegment = {
   chats: ChatDialog[];
+  notification: NotificationContent;
   endRepoID: string /* ID of the comparing repository  */;
 };
+
+export type ScenarioContent = {
+  nameId: string;
+  name: string;
+  segment: ScenarioSegment[];
+}
