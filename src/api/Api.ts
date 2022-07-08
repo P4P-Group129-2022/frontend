19f964d8b4748fbee96c5bc082e46b9f12c9f0ac
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-07-09 11:07:15 +1200

    Connect git status actions with backend

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -4,12 +4,22 @@ import { ScenarioContent } from "../types/ScenarioTypes";
 
 const API_ENDPOINT = `${process.env.REACT_APP_BACKEND_ENDPOINT}`;
 
-export const getNotificationByName: (notificationName: string) =>
-  Promise<AxiosResponse<{ notificationFromDB: NotificationContent[] }>> = async (notificationName: string) => {
-  return await axios.get(`${API_ENDPOINT}/api/notification/${notificationName}`);
-};
+export const getNotificationByName:
+  (notificationName: string) => Promise<AxiosResponse<{ notificationFromDB: NotificationContent[] }>> =
+  async (notificationName: string) =>
+    await axios.get(`${API_ENDPOINT}/api/notification/${notificationName}`);
 
-export const getScenarioByNameId: (scenarioNameId: string) =>
-  Promise<AxiosResponse<{ scenarioFromDB: ScenarioContent }>> = async (scenarioNameId: string) => {
-  return await axios.get<{ scenarioFromDB: ScenarioContent }>(`${API_ENDPOINT}/api/scenario/${scenarioNameId}`);
-};
+export const getScenarioByNameId:
+  (scenarioNameId: string) => Promise<AxiosResponse<{ scenarioFromDB: ScenarioContent }>> =
+  async (scenarioNameId: string) =>
+    await axios.get<{ scenarioFromDB: ScenarioContent }>(`${API_ENDPOINT}/api/scenario/${scenarioNameId}`);
+
+export const getRepoStatusForScenario:
+  (scenarioNameId: string) => Promise<AxiosResponse<{ status: string }>> =
+  async (scenarioNameId: string) =>
+    await axios.get<{ status: string }>(`${API_ENDPOINT}/api/git/status`, {
+      params: {
+        scenarioNameId
+        // username
+      }
+    });
--- src/utils/TerminalCommandProcessor.ts
+++ src/utils/TerminalCommandProcessor.ts
@@ -1,30 +1,82 @@
 import { ConsolePrint } from "../types/TerminalTypes";
+import { getRepoStatusForScenario } from "../api/Api";
+import { TERMINAL_COLORS } from "../theme/colors";
 
-function processGitStatus(): ConsolePrint {
-  // please implement these when you connect it with the backend.
-  return { input: "git status", output: `"git status" not implemented yet.` };
+async function processGitStatus(): Promise<ConsolePrint> {
+  const response = await getRepoStatusForScenario("test1");
+
+  const onBranchMain = `On branch main`;
+  const nothingToCommit = `nothing to commit, working tree clean`;
+  const changesNotStaged = `Changes not staged for commit:`;
+  const changesToCommit = `Changes to be committed:`;
+  const useGitAdd = `  (use "git add <file>..." to update what will be committed)`;
+  const useGitRestoreUnstaged = `  (use "git restore <file>..." to discard changes in working directory)`;
+  const useGitRestoreStaged = `  (use "git restore --staged <file>..." to unstage)`;
+  const modifiedMain = `        modified:   main.py`;
+  const noChangesAddedToCommit = `\nno changes added to commit (use "git add" and/or "git commit -a")`;
+
+  switch (response.data.status) {
+    case "modified":
+      return {
+        input: "git status",
+        output: [
+          { value: onBranchMain },
+          { value: changesToCommit },
+          { value: useGitRestoreStaged },
+          { value: modifiedMain, color: TERMINAL_COLORS.green },
+        ]
+      };
+    case "*modified":
+      return {
+        input: "git status",
+        output: [
+          { value: onBranchMain },
+          { value: changesNotStaged },
+          { value: useGitAdd },
+          { value: useGitRestoreUnstaged },
+          { value: modifiedMain, color: TERMINAL_COLORS.red },
+          { value: noChangesAddedToCommit },
+        ]
+      };
+    case "unmodified":
+      return {
+        input: "git status",
+        output: [
+          { value: onBranchMain },
+          { value: nothingToCommit },
+        ]
+      };
+    default:
+      return {
+        input: "git status",
+        output: [
+          { value: "Unknown isomorphic git status return value:" },
+          { value: `Value: ${response.data.status}` },
+        ]
+      };
+  }
 }
 
-function processGitAdd(fileNames: string[]): ConsolePrint {
+async function processGitAdd(fileNames: string[]): Promise<ConsolePrint> {
   // please implement these when you connect it with the backend.
-  return { input: "git add", output: `"git add" not implemented yet.` };
+  return { input: "git add", output: [{ value: `"git add" not implemented yet.` }] };
 }
 
-function processGitCommit(args: string[]): ConsolePrint {
+async function processGitCommit(args: string[]): Promise<ConsolePrint> {
   // please implement these when you connect it with the backend.
-  return { input: "git commit", output: `"git commit" not implemented yet.` };
+  return { input: "git commit", output: [{ value: `"git commit" not implemented yet.` }] };
 }
 
-function processGitPush(args: string[]): ConsolePrint {
+async function processGitPush(args: string[]): Promise<ConsolePrint> {
   // please implement these when you connect it with the backend.
-  return { input: "git push", output: `"git push" not implemented yet.` };
+  return { input: "git push", output: [{ value: `"git push" not implemented yet.` }] };
 }
 
-export function processCommands(command: string): ConsolePrint {
+export async function processCommands(command: string): Promise<ConsolePrint> {
   const [commandType, gitCommand, ...args] = command.split(/\s+(?=(?:[^'"]*['"][^'"]*['"])*[^'"]*$)/);
 
   if (commandType !== "git") {
-    return { input: command, output: `"${command}" is not a git command.` };
+    return { input: command, output: [{ value: `"${command}" is not a git command.` }] };
   } else {
     switch (gitCommand) {
       case "status":
@@ -38,7 +90,9 @@ export function processCommands(command: string): ConsolePrint {
       default:
         return {
           input: command,
-          output: `"${commandType} ${gitCommand}" is either not a valid git command or it hasn't been implemented yet.`
+          output: [{
+            value: `"${commandType} ${gitCommand}" is either not a valid git command or it hasn't been implemented yet.`
+          }]
         };
     }
   }
