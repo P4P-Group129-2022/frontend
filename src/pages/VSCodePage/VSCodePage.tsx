34f78ddecc6804e5a02231b04236a33de2a1226e
hpar461 <68981068+hpar461@users.noreply.github.com>
2022-07-20 23:42:57 +1200

    Retrieve and modify files from the backend (#32)
    * Make vscode to use files fetched from the backend.
    
    * Add file save fail snackbar.
    
    * Remove redundant dummy files.
    
    * Add save and modified identifier

--- src/pages/VSCodePage/VSCodePage.tsx
+++ src/pages/VSCodePage/VSCodePage.tsx
@@ -1,70 +1,85 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Alert, Box, Breadcrumbs, Snackbar, Typography } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import CloseIcon from "@mui/icons-material/Close";
 import Editor from "@monaco-editor/react";
 import AppWindowFrame from "../../components/AppWindowFrame";
-import { VSCODE_COLORS } from "../../theme/colors";
 import VSCodeSidebar from "../../components/VSCodeSidebar";
 import VSCodeExplorer from "../../components/VSCodeExplorer";
-import { Box, Breadcrumbs, Typography } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import HTTPStatusCode from "../../constants/HTTPStatusCode";
+import { modifyFile, retrieveFile } from "../../api/Api";
+import { VSCODE_COLORS } from "../../theme/colors";
 import { File } from "../../types/FileTypes";
 
-const dummyfiles: File[] = [
-  {
-    name: "new folder",
-    isFolder: true,
-    folderContents: [
-      {
-        name: "new folder2",
-        isFolder: true,
-        folderContents: [
-          {
-            name: "new file3.md",
-            isFolder: false,
-            contents: "hhehe",
-          }
-        ]
-      },
-      {
-        name: "new file.md",
-        isFolder: false,
-        contents: "hhehe",
-      },
-      {
-        name: "new file2.md",
-        isFolder: false,
-        contents: "hhehe",
-      },
-    ],
-  },
-  {
-    name: "dummy.txt",
-    isFolder: false,
-    contents: "hehe xd hello world",
-  },
-  {
-    name: "main.py",
-    isFolder: false,
-    contents: "def main():\n    print('hello world')",
-  }
-];
-
 function VSCodePage() {
-  const [code, setCode] = useState(dummyfiles[2].contents);
-  const [fileName, setFileName] = useState("main.py");
+  const [modified, setModified] = useState(false);
+  const [saved, setSaved] = useState(true);
+  const [saveFailSnackbarOpen, setSaveFailSnackbarOpen] = useState(false);
+  const [files, setFiles] = useState<File[]>([]);
+  const [code, setCode] = useState<string>();
+  const [fileName, setFileName] = useState<string>();
+
+  useEffect(() => {
+    retrieveFile("testUser")
+      .then(res => res.data)
+      .then(data => {
+        console.log(data);
+        setFiles(data);
+        setCode(data[0].contents);
+        setFileName(data[0].name);
+      });
+  }, []);
 
   const breadcrumbs = [
     <Typography key={1} color={"inherit"}>codebase</Typography>,
     <Typography key={2} color={"inherit"}>{fileName}</Typography>
   ];
 
-  const preventSave = (e: React.KeyboardEvent<HTMLDivElement>) =>
-    e.key === "s" && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.preventDefault();
+  const handleSave = async (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "s" && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+      e.preventDefault();
+
+      if (code) {
+        try {
+          const res = await modifyFile("testUser", code);
+          if (res.status === HTTPStatusCode.NO_CONTENT) {
+            setModified(code !== files[0].contents);
+            setSaved(true);
+            console.log("file saved to the backend repo.");
+          } else {
+            console.log("file save failed.");
+            console.log("status: ", res.status);
+            setSaveFailSnackbarOpen(true);
+          }
+        } catch (err) {
+          console.log("file save failed.");
+          console.log("error: ", err);
+          setSaveFailSnackbarOpen(true);
+        }
+      }
+    }
+  };
+
+  const handleChange = (newValue: string | undefined) => {
+    if (newValue) {
+      setCode(newValue);
+      setSaved(false);
+    }
+  };
+
+  const handleSaveFailSnackbarClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setSaveFailSnackbarOpen(false);
+  };
 
   return (
     <AppWindowFrame frameColor={VSCODE_COLORS.frame} title={`${fileName} — LGI Codebase`}>
       <VSCodeSidebar />
-      <VSCodeExplorer files={dummyfiles} />
+      <VSCodeExplorer files={files} />
       <Box
         display={"flex"}
         flexDirection={"column"}
@@ -72,7 +87,7 @@ function VSCodePage() {
         flexGrow={1}
         color={"white"}
         bgcolor={VSCODE_COLORS.textarea}
-        onKeyDown={preventSave}
+        onKeyDown={handleSave}
       >
         <Box
           display={"flex"}
@@ -92,11 +107,19 @@ function VSCodePage() {
               fontSize={"0.8rem"}
               fontWeight={600}
               marginRight={1}
+              color={modified ? VSCODE_COLORS.changedText : VSCODE_COLORS.explorerText}
             >
               {fileName}
+              {modified && (
+                <span style={{ color: `${VSCODE_COLORS.changedText}BB`, fontWeight: "bold", marginLeft: "0.4rem" }}>M</span>
+              )}
             </Typography>
 
-            <CloseIcon fontSize={"small"} />
+            {saved ? (
+              <CloseIcon fontSize={"small"} />
+            ) : (
+              <FiberManualRecordIcon sx={{ width: "0.75rem", height: "0.75rem" }} />
+            )}
           </Box>
         </Box>
 
@@ -119,8 +142,23 @@ function VSCodePage() {
           defaultLanguage="python"
           theme="vs-dark"
           defaultValue={code}
-          onChange={(value) => value && setCode(value)}
+          onChange={handleChange}
         />
+
+        <Snackbar
+          open={saveFailSnackbarOpen}
+          autoHideDuration={3000}
+          onClose={handleSaveFailSnackbarClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        >
+          <Alert
+            severity={"error"}
+            onClose={handleSaveFailSnackbarClose}
+            sx={{ width: "100%" }}
+          >
+            File save failed! Please try again.
+          </Alert>
+        </Snackbar>
       </Box>
     </AppWindowFrame>
   );
