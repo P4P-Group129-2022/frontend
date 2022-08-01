9791743d1d328fdc1919c32551d8bd896ff0a8df
Dave Shin <100652749+hiin3d55@users.noreply.github.com>
2022-08-01 17:51:50 +1200

    Implemented the UI for the OrgInvitePage.

--- src/pages/OrgInvitePage/OrgInvitePage.tsx
+++ src/pages/OrgInvitePage/OrgInvitePage.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Box, Button, TextField, Typography} from "@mui/material";
-import { styled } from "@mui/material/styles";
-import { useNavigate } from "react-router-dom";
+import {styled} from "@mui/material/styles";
+import {useNavigate} from "react-router-dom";
 import {inviteToOrganization} from "../../api/Api";
+import {NotificationContext} from "../../contexts/NotificationContextProvider";
 
 const MainContainer = styled(Box)({
     display: "flex",
@@ -14,11 +15,24 @@ const MainContainer = styled(Box)({
 });
 
 function ScenarioSelectPage() {
+    const {showNotification} = useContext(NotificationContext);
+    const navigate = useNavigate();
+
     const [username, setUsername] = React.useState("");
     const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setUsername(event.target.value);
     };
-    const navigate = useNavigate();
+
+    const sendInviteToOrganization = async (username: string) => {
+        await inviteToOrganization(username);
+        showNotification({
+            title: "Check GitHub or your email!",
+            message: "Please check GitHub or your email for the invite to our GitHub organisation. " +
+                "If you did not get an invite, please check that you have entered in your correct GitHub username " +
+                "When you have checked that you have been successfully invited, please start with scenario 1! :D",
+            imageSrc: ""
+        });
+    };
 
     return (
         <MainContainer>
@@ -37,7 +51,7 @@ function ScenarioSelectPage() {
                 <Button
                     variant={"contained"}
                     onClick={async () => {
-                        await inviteToOrganization(username);
+                        await sendInviteToOrganization(username);
                     }}
                 >
                     Submit
