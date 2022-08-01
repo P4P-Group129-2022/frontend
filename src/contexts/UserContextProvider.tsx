b8afa0c7ee8288aebcca0d0592e08584a8373965
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-08-01 12:09:59 +1200

    Implement user context and relevant persisted states

---
+++ src/contexts/UserContextProvider.tsx
@@ -0,0 +1,61 @@
+import { createContext, useEffect, useState } from "react";
+import { User } from "../types/UserTypes";
+import { User as FbUser, onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/config";
+import { RestEndpointMethodTypes } from "@octokit/rest";
+import { useAccessTokenState, useUserState } from "../hooks/usePersistedState";
+
+type UserContextType = {
+  user: User | undefined,
+  accessToken: string | undefined,
+  loginToContext: (user: GitHubUser, email: string, accessToken: string) => void,
+  logoutFromContext: () => void,
+}
+
+type Props = {
+  children: JSX.Element;
+}
+
+type GitHubUser = RestEndpointMethodTypes["users"]["getAuthenticated"]["response"]["data"];
+
+const UserContext = createContext<UserContextType>({
+  user: { email: "", name: "", username: "" },
+  accessToken: "",
+  loginToContext: () => {},
+  logoutFromContext: () => {},
+});
+
+const EMPTY_USER: User = { email: "", name: "", username: "" };
+
+function UserContextProvider({ children }: Props) {
+  const [user, setUser] = useUserState(EMPTY_USER);
+  const [accessToken, setAccessToken] = useAccessTokenState();
+
+  const extractUserInformation = ({ login, name, }: GitHubUser, email: string): User =>
+    ({ email: email ?? "", name: name ?? login, username: login });
+
+  const loginToContext = (user: GitHubUser, email: string, accessToken: string) => {
+    setAccessToken(accessToken);
+    setUser(extractUserInformation(user, email));
+  };
+
+  const logoutFromContext = () => {
+    setUser(EMPTY_USER);
+    setAccessToken("");
+  };
+
+  const context: UserContextType = {
+    user,
+    accessToken,
+    loginToContext,
+    logoutFromContext,
+  };
+
+  return (
+    <UserContext.Provider value={context}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export { UserContext, UserContextProvider };
