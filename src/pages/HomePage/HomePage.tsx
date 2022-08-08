ac4fdf2cf3a964024bfbb196bc7ca7e37bd8b3ec
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-08-09 00:18:10 +1200

    Redesign home page and connect instruction dialog

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,35 @@
 import React, { useContext } from "react";
-import CustomButton from "../../components/Buttons";
-import { Box, Typography } from "@mui/material";
+import { Avatar, Box, Typography, Button as MuiButton, styled, ButtonProps } from "@mui/material";
 import classes from "./HomePage.module.css";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContextProvider";
 import { useLogin } from "../../hooks/useLogin";
 import { useLogout } from "../../hooks/useLogout";
 import LinkToTestsDialog from "../../components/LinkToTestsDialog";
+import InstructionsDialog from "../../components/InstructionsDialog";
+
+const Button = styled((props: ButtonProps) => (
+  <MuiButton color={"secondary"} size={"large"} variant={"contained"} {...props} />
+))({
+  fontSize: "1.125rem",
+  margin: "4px 12px",
+  borderRadius: 1000,
+});
+
+const GreetingBody = styled(Typography)({
+  textAlign: "center",
+  whiteSpace: "pre-wrap",
+  fontWeight: "300",
+  fontSize: "1.25rem",
+});
 
 function HomePage() {
-  const { user } = useContext(UserContext);
-  const { login, error, isPending } = useLogin();
+  const { user, loggedIn } = useContext(UserContext);
+  const { login } = useLogin();
   const { logout } = useLogout();
   const [open, setOpen] = React.useState(false);
+  const [openInstructions, setOpenInstructions] = React.useState(false);
+  const [isHovering, setIsHovering] = React.useState(false);
 
   const navigate = useNavigate();
 
@@ -28,61 +45,88 @@ function HomePage() {
     setOpen(false);
   };
 
-  console.log("user", user);
+  const handleOpenInstruction = () => {
+    setOpenInstructions(true);
+  };
+
+  const handleCloseInstruction = () => {
+    setOpenInstructions(false);
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        width: "100vw",
-      }}
-    >
-      <Typography
-        variant="h1"
-        textAlign="center"
-        marginBottom={20}
+    <Box>
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        alignItems="center"
+        marginTop={1}
       >
-        Let's Git it
-      </Typography>
-      <Box className={classes.ButtonContainer}>
-        <CustomButton onClick={handleSelectScenario}>
-          Select Scenario
-        </CustomButton>
-        <CustomButton onClick={handleStartTests}>
-          Start Tests
-        </CustomButton>
-        <LinkToTestsDialog open={open} onClose={handleClose} />
-        <CustomButton onClick={login}>
-          Login
-        </CustomButton>
-        <CustomButton onClick={logout}>
-          Logout
-        </CustomButton>
+        {loggedIn ? (
+          <>
+            <Avatar src={user.avatarUrl} sx={{ border: "1px solid #00000050", marginRight: 1 }} />
+            <Typography variant="h5" align="center" color={"#000000AA"}>
+              Username: {user.name}
+            </Typography>
+            <Button variant={"contained"} size={"medium"} onClick={logout}>
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Button variant={"contained"} size={"medium"} onClick={login}>
+            Login
+          </Button>
+        )}
       </Box>
-      {!!user && (
-        <Box
-          display={"flex"}
-          flexDirection={"row"}
-          marginTop={10}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "20vh",
+        }}
+      >
+        <Typography
+          variant="h1"
+          textAlign="center"
+          marginBottom={4}
+          fontWeight={600}
         >
-          {/*<img src={user.avatar ?? ""} alt="gitit" height={160} />*/}
-          <Box
-            display={"flex"}
-            flexDirection={"column"}
-            marginLeft={10}
-          >
-            <Typography variant={"body1"}>username: {user.username}</Typography>
-            <Typography variant={"body1"}>email: {user.email}</Typography>
-            <Typography variant={"body1"}>name: {user.name}</Typography>
-          </Box>
+          Let's Git it
+        </Typography>
+        <GreetingBody marginBottom={4}>
+          A scenario-based simulation tool to help you learn git in an industry-like way.<br />
+          Please read this <span onClick={handleOpenInstruction} onMouseEnter={handleMouseEnter}
+                                 onMouseLeave={handleMouseLeave} style={{
+          cursor: isHovering ? "pointer" : "default",
+          textDecoration: "underline",
+          fontWeight: "bold",
+        }}>instruction</span> before you start!
+        </GreetingBody>
+        <GreetingBody sx={{ marginBottom: "10vh", fontSize: "1rem", color: "#00000080" }}>
+          FYI: Scenario will be enabled after you complete a pre-survey.<br />
+          If you didn't know what pre-survey was, perhaps it's good idea to <span
+          style={{ fontWeight: 400, color: "black" }}>READ THE INSTRUCTION!</span>
+        </GreetingBody>
+        <InstructionsDialog open={openInstructions} onClose={handleCloseInstruction} />
+        <Box className={classes.ButtonContainer}>
+          <Button variant={"contained"} disabled={!user.completedPreTest} onClick={handleSelectScenario}>
+            Select Scenario
+          </Button>
+          <Button variant={"contained"} disabled={!loggedIn} onClick={handleStartTests}>
+            Start Tests
+          </Button>
+          <LinkToTestsDialog open={open} onClose={handleClose} />
         </Box>
-      )}
-      {error !== undefined && (<Typography variant={"h3"} color={"#ff0000"}>{error}</Typography>)}
-      {isPending && (<Typography variant={"h3"} color={"#202020"}>Loading...</Typography>)}
+      </Box>
     </Box>
   );
 }
