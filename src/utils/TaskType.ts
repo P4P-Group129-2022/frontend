fc8456d44d7987e313c5c5f33908446986feaabb
Dave Shin <100652749+hiin3d55@users.noreply.github.com>
2022-08-12 11:52:39 +1200

    Added additional task types.

--- src/utils/TaskType.ts
+++ src/utils/TaskType.ts
@@ -2,8 +2,11 @@ export enum TaskType {
     INITIAL = "INITIAL",
     FINAL = "FINAL",
     SAVE = "SAVE",
+    ADD = "ADD",
+    COMMIT = "COMMIT",
     PUSH = "PUSH",
     PR = "PR",
+    BRANCH = "BRANCH",
     CHECKOUT = "CHECKOUT",
     REBASE = "REBASE",
 }
