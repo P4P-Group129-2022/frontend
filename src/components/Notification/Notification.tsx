e37565a371a5b19a0d94d7ce3c7c5502447eb7a1
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-08-15 17:16:48 +1200

    Update notification component

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,8 +1,9 @@
-import { useContext, useState } from "react";
-import { Snackbar, SnackbarContent, Typography } from "@mui/material";
+import { SyntheticEvent, useContext, useState } from "react";
+import { Box, IconButton, Snackbar, SnackbarContent, Typography } from "@mui/material";
 import { NotificationContent } from "../../types/NotificationTypes";
 import { NotificationContext } from "../../contexts/NotificationContextProvider";
 import { styled } from "@mui/material/styles";
+import CloseIcon from "@mui/icons-material/Close";
 
 const NotificationHeader = styled(Typography)({
   fontSize: "1.25rem",
@@ -12,7 +13,14 @@ const NotificationHeader = styled(Typography)({
 const NotificationBody = styled(Typography)({
   display: "flex",
   fontSize: "1rem",
+  fontWeight: "400",
   justifyContent: "center",
+});
+
+const NotificationIcon = styled("img")({
+  height: "3.5rem",
+  marginRight: "0.5rem",
+  filter: "drop-shadow(2px 2px 8px #00000030)"
 });
 
 function Notification({ title, message, imageSrc }: NotificationContent) {
@@ -21,7 +29,13 @@ function Notification({ title, message, imageSrc }: NotificationContent) {
 
   console.log("notification open: ", { title, message, imageSrc });
 
-  const closeNotification = () => setOpen(false);
+  const closeNotification = (event: SyntheticEvent | Event, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
   const imageUrl = (name: string) => `${process.env.PUBLIC_URL}/assets/icons/${name}.webp`;
 
   return (
@@ -34,23 +48,37 @@ function Notification({ title, message, imageSrc }: NotificationContent) {
       <SnackbarContent
         style={{
           color: "#000000",
-          backgroundColor: "#FFFFFF",
-          display: "flex",
-          flexDirection: "column",
+          backgroundColor: "#EAEAEAAA",
           maxWidth: "30rem",
+          backdropFilter: "blur(18px)",
         }}
         message={
-          <>
-            <NotificationHeader>
-              {title}
-            </NotificationHeader>
-            <NotificationBody>
-              {message}
-              <span>
-                {imageSrc && <img src={imageUrl(imageSrc)} alt={imageSrc} height="52px" />}
-              </span>
-            </NotificationBody>
-          </>
+          <Box
+            display={"flex"}
+            flexDirection={"row"}
+            alignItems={"center"}
+          >
+            {imageSrc && <NotificationIcon src={imageUrl(imageSrc)} alt={`${imageSrc} icon`} />}
+            <Box
+              display={"flex"}
+              flexDirection={"column"}
+            >
+              <NotificationHeader>
+                {title}
+              </NotificationHeader>
+              <NotificationBody>
+                {message}
+              </NotificationBody>
+            </Box>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={closeNotification}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </Box>
         }
       />
     </Snackbar>
