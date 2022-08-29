634ebd60848790e2f67b7ad9f5e9bee8686eda84
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-08-29 17:08:22 +1200

    Correct outputs for git branch & checkout

--- src/hooks/useTerminalCommandProcessor.ts
+++ src/hooks/useTerminalCommandProcessor.ts
@@ -367,7 +367,7 @@ export const useTerminalCommandProcessor = () => {
           output: [
             { value: "Error creating branch.", color: TERMINAL_COLORS.red },
             ...(e instanceof AxiosError && !!e.response && e.response.status === HTTPStatusCode.INTERNAL_SERVER_ERROR && e.response.data?.message === "AlreadyExistsError" ? [
-              { value: `Branch already exists.` },
+              { value: `fatal: A branch named '${branchName}' already exists.` },
               { value: `hint: Use "git checkout ${branchName}" to check out to that branch or try again with a different name.` },
             ] : [{ value: "Unknown error occurred. Please try again." }]),
           ]
@@ -377,8 +377,14 @@ export const useTerminalCommandProcessor = () => {
       return {
         input,
         output: [
-          { value: "In actual git interface, this would result in listing out currently available branches." },
-          { value: "However, this feature is not implemented in this program." },
+          {
+            value: "In actual git interface, this would result in listing out currently available branches.",
+            color: TERMINAL_COLORS.yellow
+          },
+          {
+            value: "However, this feature is not implemented in this program.",
+            color: TERMINAL_COLORS.yellow
+          },
           { value: "hint: Use 'git branch <branchName>' to create a new branch." },
         ]
       };
@@ -393,24 +399,24 @@ export const useTerminalCommandProcessor = () => {
 
     if (branchOrArg.startsWith("-") || branchOrArg.startsWith("--")) {
       if (branchOrArg === "-b") {
+        isCreateBranch = true;
+        branchName = newBranchName;
+
         try {
-          await branch(username, newBranchName);
+          await branch(username, branchName);
         } catch (e) {
           console.log("Error occurred while making a new branch for git checkout -b", e);
           return {
             input,
             output: [
-              { value: "Error creating branch.", color: TERMINAL_COLORS.red },
+              { value: "Error creating a branch to check out.", color: TERMINAL_COLORS.red },
               ...(e instanceof AxiosError && !!e.response && e.response.status === HTTPStatusCode.INTERNAL_SERVER_ERROR && e.response.data?.message === "AlreadyExistsError" ? [
-                { value: `Branch already exists.` },
-                { value: `hint: Use "git checkout ${newBranchName}" to check out to that branch or try again with a different branch name.` },
+                { value: `fatal: A branch named '${branchName}' already exists.` },
+                { value: `hint: Use "git checkout ${branchName}" to check out to that branch or try again with a different branch name.` },
               ] : [{ value: "Unknown error occurred while creating a new branch. Please try again." }]),
             ]
           };
         }
-
-        branchName = newBranchName;
-        isCreateBranch = true;
       } else {
         return {
           input,
@@ -433,7 +439,7 @@ export const useTerminalCommandProcessor = () => {
         checkAndAdvanceScenarioSegment(isCreateBranch ? TaskType.BRANCH_CHECKOUT : TaskType.CHECKOUT);
         return {
           input,
-          output: [noOutput]
+          output: [{ value: `Switched to branch '${branchName}'` }]
         };
       } catch (e) {
         console.log("Error occurred while running git checkout", e);
@@ -444,6 +450,7 @@ export const useTerminalCommandProcessor = () => {
             ...(e instanceof AxiosError && !!e.response && e.response.status === HTTPStatusCode.INTERNAL_SERVER_ERROR && e.response.data?.message === "NotFoundError" ? [
               { value: `Branch ${branchName} not found.` },
               { value: `hint: Use "git branch <branchName>" to first create a branch.`, },
+              { value: `hint: Use "git checkout -b <branchName>" to create a new branch and check out.`, },
             ] : [{ value: "Unknown error occurred. Please try again." }]),
           ]
         };
